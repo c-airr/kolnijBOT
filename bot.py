b69a6fe757be32caaa0dd8953885d5c17a2a1504
bartek <108690485+c-airr@users.noreply.github.com>
2025-08-18 21:17:37 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,92 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+from adb_bot import pierdzielnij_numer
+import asyncio
+import json
+import os
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+TOKEN_PATH = os.path.join(SCRIPT_DIR, "token.json")
+
+with open(TOKEN_PATH, "r", encoding="utf-8") as f:
+    data = json.load(f)
+TOKEN = data.get("token")
+if not TOKEN:
+    raise ValueError("Nie znaleziono tokena w pliku token.json")
+
+ALLOWED_USERS = [WpiszTuSwojID]
+BLOKOWANE_NUMERY = {"997","998","999","112","986","911","000","111","110","119","113","122","133","144","118","117","15","17","18","101","102","103","104","995","996","987","992","991","993","994"}
+
+
+def sprawdz_numer(numer: str) -> bool:
+    return numer not in BLOKOWANE_NUMERY
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+with open("shortcuts.json", "r", encoding="utf-8") as f:
+    shortcuts = json.load(f)
+
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print(f"check {client.user} - ready")
+
+@app_commands.allowed_installs(guilds=True, users=True)
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@app_commands.user_install
+@tree.command(name="kolnij", description="kolnij na numer")
+async def kolnij_command(interaction: discord.Interaction, numer: str):
+    if any(zakazany in numer for zakazany in ["+", "#", "*"]):
+        await interaction.response.send_message("nie ma kurwa dzwonienia do chinduskuf gamoniu", ephemeral=True)
+        return
+
+    if not sprawdz_numer(numer):
+        await interaction.followup.send("ja ci kurwa dam zaraz guwniarzu mały")
+        return
+    if interaction.user.id not in ALLOWED_USERS:
+        await interaction.response.send_message("oj byczku nie masz whitelisty", ephemeral=True)
+        return
+
+    await interaction.response.send_message(
+        f"No i dobrza dzwonimy na {numer} zwaituj chile...",
+        ephemeral=False
+    )
+    try:
+        pierdzielnij_numer(numer, sekundy=6)
+        await interaction.followup.send(f"✅ Misja wykonana ✅, potwierdzenie przelewu: {numer}")
+    except Exception as e:
+        await interaction.followup.send(f"12 giga szpontu poszło sie pierdolic: {e}")
+        
+
+choices = [app_commands.Choice(name=name, value=name) for name in shortcuts.keys()]
+
+@app_commands.allowed_installs(guilds=True, users=True)
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@app_commands.user_install
+@tree.command(name="kolnijct", description="kolnij kontakt")
+@app_commands.choices(target=choices)
+async def kolnij_command(interaction: discord.Interaction, target: str):
+    if interaction.user.id not in ALLOWED_USERS:
+        await interaction.response.send_message("oj byczku nie masz whitelisty", ephemeral=True)
+        return
+
+    numer = shortcuts.get(target.lower(), target)
+
+    await interaction.response.send_message(
+        f"juz odpalam AJI i dzwonimy do ({target}) przydzwanianie...",
+        ephemeral=False
+    )
+    try:
+        pierdzielnij_numer(numer, sekundy=6)
+        await interaction.followup.send(f"✅ Misja wykonana ✅, potwierdzenie zakupu robuxow: {target}")
+    except Exception as e:
+        await interaction.followup.send(f"12 giga szpontu poszło sie pierdolic: {e}")
+
+client.run(TOKEN)
